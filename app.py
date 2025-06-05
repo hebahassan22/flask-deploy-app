1ee18759698b422e4801adaa2683df2e292e5584
Heba Hassan <hebahas105@gmail.com>
2025-06-05 23:01:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
- 
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, this is a deployed Flask app!"
+    return "🚀 Hello! This is my deployment pipeline test Flask app."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render requires the app to run on host=0.0.0.0 and a dynamic port
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
